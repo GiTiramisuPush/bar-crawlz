1cc9d06586a2421a81330b6a28be1feafdbb5cb4
LionSnarl <60535870+LionSnarl@users.noreply.github.com>
2021-01-21 13:25:49 -0800

    reactstrap, router-dom, devise, and rails commands fully complete

---
+++ app/javascript/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component }  from "react"
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import AboutUs from './components/AboutUs'
+
+class App extends Component {
+  render () {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default App
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do  
+  
+  devise_for :users
+  get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
+  
+  root to: 'home#index'
 end
