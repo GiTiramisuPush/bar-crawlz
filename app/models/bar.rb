a20d26fdbe5456bf55ddd8a37b4f8ca9617e813a
LionSnarl <60535870+LionSnarl@users.noreply.github.com>
2021-01-26 11:04:15 -0800

    Controller Methods needed

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,7 +1,7 @@
 class Bar < ApplicationRecord
     # Associations
-    has_many :crawls, through:CrawlBar
-    has_many :CrawlBar
+    has_many :crawls, through: :crawl_bars
+    has_many :crawl_bars
     # Validations
     validates :yelp_id, :name, :image_url, :categories, :rating, :address1, :city, :zip_code, :country, :display_address, :phone, :display_phone, presence: true
 end
--- app/models/crawl.rb
+++ app/models/crawl.rb
@@ -1,8 +1,8 @@
 class Crawl < ApplicationRecord
     # Associations
     belongs_to :user
-    has_many :CrawlBar
-    has_many :bars, through: :CrawlBar
+    has_many :crawl_bars
+    has_many :bars, through: :crawl_bars
     # Validations
     validates :title, :user_id, presence: true
 end
