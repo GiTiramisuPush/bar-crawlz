d02e836b2ad2179a566233f3849a0b80b06d6799
LionSnarl <60535870+LionSnarl@users.noreply.github.com>
2021-01-27 10:15:39 -0800

    all 18 tests passing

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,11 +1,23 @@
 class BarsController < ApplicationController
 
+
+    
 def create
-     binding.irb
-    crawl = Crawl.find(bar_params[:crawl_id])
+    crawl = Crawl.find(crawl_params[:id])
     bar = crawl.bars.create(bar_params)
     if bar.valid?
         render json: bar
+    end
+    # binding.irb
+end
+
+def destroy
+    bar = Bar.find(params[:id])
+    bar.destroy
+    if bar.valid?
+        render json: bar
+    else
+        render json: bar.errors, status: unprocessable_entity
     end
 end
 
@@ -13,7 +25,11 @@ private
 
 def bar_params
     params.require(:bar).permit(:yelp_id, :name, :image_url, :categories, :rating, :address1, :address2, :address3, :city, :zip_code, :country, :display_address, :phone, :display_phone)
-end
+    end
+
+def crawl_params
+    params.require(:crawl).permit(:id)
+    end
 
 end
 
--- spec/requests/bars_request_spec.rb
+++ spec/requests/bars_request_spec.rb
@@ -37,15 +37,53 @@ RSpec.describe "Bars", type: :request do
             phone: '+14089020987', 
             display_phone: '1(408)903-2343'
             },
-            crawl_id: crawl.id
+            crawl: {
+                id: crawl.id
+            }
         }
     
     post '/bars', params: bar_params
     bar = Bar.first
     expect(bar.yelp_id).to eq 'ahfasdf23'
+    crawl2 = Crawl.find(crawl.id)
+    expect(crawl2.bars.first.id).to eq bar.id
     # expect(bar.user_id).to eq user.id
     expect(response).to have_http_status(200)
     end
+
+    it 'Adds a bar to a crawl' do
+        bar_params = {
+            bar: {
+            yelp_id: 'ahfasdf23', 
+            name: 'Bills bar', 
+            image_url: 'https://wwee.com', 
+            categories: 'bars all', 
+            rating: 3.9, 
+            address1: '42 WayBad Ave.', 
+            city: 'San Clemente', 
+            zip_code: '98034', 
+            country: 'USA', 
+            display_address: '42 WayBad Ave', 
+            phone: '+14089020987', 
+            display_phone: '1(408)903-2343'
+            },
+            crawl: {
+                id: crawl.id
+            }
+        }
+    
+    post '/bars', params: bar_params
+    bar = Bar.first
+    delete "/bars/#{bar.id}"
+    bars = Bar.all
+    expect(bars).to be_empty
+    # crawl2 = Crawl.find(crawl.id)
+    # expect(crawl2.bars.first.id).to be_empty
+    # expect(bar.user_id).to eq user.id
+    expect(response).to have_http_status(200)
+    end
+
+    # 
 
 
 end
