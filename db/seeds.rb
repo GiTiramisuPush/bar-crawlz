24c14cfd052760bbe7d96e6208f262cc914cef57
LionSnarl <60535870+LionSnarl@users.noreply.github.com>
2021-01-28 11:28:29 -0800

    consistent indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,87 +62,87 @@ bars = [
     },
 
     {
-        yelp_id: "mQnqJvSIiAMrSMqLeNbbVg",
-        name: "Sherman Hops",
-        image_url: "https://s3-media4.fl.yelpcdn.com/bphoto/6PqsvP2e0BzPHjdcCfO77Q/o.jpg",
-        url: "https://www.yelp.com/biz/sherman-hops-los-angeles?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw",
-        rating: 5.0,
-        address1: "14311 Ventura Blvd",
-        address2: "",
-        address3: null,
-        city: "Los Angeles",
-        zip_code: "91423",
-        country: "US",
-        state: "CA",
-        display_address:  "14311 Ventura Blvd, Los Angeles, CA 91423",
-        phone: "+18582373991",
-        display_phone: "(619) 226-6333"
+    yelp_id: "mQnqJvSIiAMrSMqLeNbbVg",
+    name: "Sherman Hops",
+    image_url: "https://s3-media4.fl.yelpcdn.com/bphoto/6PqsvP2e0BzPHjdcCfO77Q/o.jpg",
+    url: "https://www.yelp.com/biz/sherman-hops-los-angeles?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw", 
+    rating: 5.0,
+    address1: "14311 Ventura Blvd",
+    address2: "",
+    address3: null,
+    city: "Los Angeles",
+    zip_code: "91423",
+    country: "US",
+    state: "CA",
+    display_address:  "14311 Ventura Blvd, Los Angeles, CA 91423",
+    phone: "+18582373991",
+    display_phone: "(619) 226-6333"
     },
        
     {
-        yelp_id: "eGhOLzFFIhrvF6QjSSpJsg",
-        alias: "phils-bbq-san-diego-2",
-        name: "Phil's BBQ",
-        image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/IijLAPbkYwMrvcMgNo_8vw/o.jpg",
-        url: "https://www.yelp.com/biz/phils-bbq-san-diego-2?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw",
-        rating: 4.5,
-        address1: "3750 Sports Arena Blvd",
-        address2: "",
-        address3: "",
-        city: "San Diego",
-        zip_code: "92110",
-        country: "US",
-        state: "CA",
-        display_address: "3750 Sports Arena Blvd, San Diego, CA 92110",
-        phone: "+16192266333",
-        display_phone: "(619) 226-6333"
+    yelp_id: "eGhOLzFFIhrvF6QjSSpJsg",
+    alias: "phils-bbq-san-diego-2",
+    name: "Phil's BBQ",
+    image_url: "https://s3-media2.fl.yelpcdn.com/bphoto/IijLAPbkYwMrvcMgNo_8vw/o.jpg",
+    url: "https://www.yelp.com/biz/phils-bbq-san-diego-2?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw",
+    rating: 4.5,
+    address1: "3750 Sports Arena Blvd",
+    address2: "",
+    address3: "",
+    city: "San Diego",
+    zip_code: "92110",
+    country: "US",
+    state: "CA",
+    display_address: "3750 Sports Arena Blvd, San Diego, CA 92110",
+    phone: "+16192266333",
+    display_phone: "(619) 226-6333"
     },
 # Bar3
     {
-        yelp_id: "J55fX2Gs-ry0XNCnEDiLNQ",
-        alias: "donut-bar-san-diego",
-        name: "Donut Bar",
-        image_url: "https://s3-media3.fl.yelpcdn.com/bphoto/-83lj_J9zeSX2FXmIRV5JQ/o.jpg",
-        url: "https://www.yelp.com/biz/donut-bar-san-diego?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw",
-        rating: 4.0,
-        address1: "631 B St",
-        address2: null,
-        address3: "",
-        city: "San Diego",
-        zip_code: "92101",
-        country: "US",
-        state: "CA",
-        display_address:"631 B St,San Diego, CA 92101",
-        phone: "+16192556360",
-        display_phone: "(619) 255-6360"
+    yelp_id: "J55fX2Gs-ry0XNCnEDiLNQ",
+    alias: "donut-bar-san-diego",
+    name: "Donut Bar",
+    image_url: "https://s3-media3.fl.yelpcdn.com/bphoto/-83lj_J9zeSX2FXmIRV5JQ/o.jpg",
+    url: "https://www.yelp.com/biz/donut-bar-san-diego?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw",
+    rating: 4.0,
+    address1: "631 B St",
+    address2: null,
+    address3: "",
+    city: "San Diego",
+    zip_code: "92101",
+    country: "US",
+    state: "CA",
+    display_address:"631 B St,San Diego, CA 92101",
+    phone: "+16192556360",
+    display_phone: "(619) 255-6360"
     },
     # Bar4
     {
-        yelp_id: "VuR1rTb3XpOr8z0r8jC3eg",
-        alias: "great-maple-hillcrest-san-diego",
-        name: "Great Maple - Hillcrest",
-        image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/n4BIvf30ZFKpS9GV0mc_tQ/o.jpg",
-        url: "https://www.yelp.com/biz/great-maple-hillcrest-san-diego?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw",
-        rating: 4.0,
-        address1: "1451 Washington St",
-        address2: "",
-        address3: "",
-        city: "San Diego",
-        zip_code: "92103",
-        country: "US",
-        state: "CA",
-        display_address: "1451 Washington St, San Diego, CA 92103",
-        phone: "+16192552282",
-        display_phone: "(619) 255-2282"
+    yelp_id: "VuR1rTb3XpOr8z0r8jC3eg",
+    alias: "great-maple-hillcrest-san-diego",
+    name: "Great Maple - Hillcrest",
+    image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/n4BIvf30ZFKpS9GV0mc_tQ/o.jpg",
+    url: "https://www.yelp.com/biz/great-maple-hillcrest-san-diego?adjust_creative=q8w7wKNQ1Uxq2hK-ANhusw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=q8w7wKNQ1Uxq2hK-ANhusw",
+    rating: 4.0,
+    address1: "1451 Washington St",
+    address2: "",
+    address3: "",
+    city: "San Diego",
+    zip_code: "92103",
+    country: "US",
+    state: "CA",
+    display_address: "1451 Washington St, San Diego, CA 92103",
+    phone: "+16192552282",
+    display_phone: "(619) 255-2282"
     }
 
 ]
 
-    # crawl: {id: crawl.id}
-    bars.each do |attr|
-        Bar.create attr
-        puts "creating bar: #{attr}"
-    end
+# crawl: {id: crawl.id}
+bars.each do |attr|
+    Bar.create attr
+    puts "creating bar: #{attr}"
+end
 
 # Creating Crawls
 crawls =[ 
