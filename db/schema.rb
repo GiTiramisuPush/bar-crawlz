62371f1415ab4f298c0c12493b7cd4bf7138b57b
LionSnarl <60535870+LionSnarl@users.noreply.github.com>
2021-01-29 13:16:01 -0800

    added URL migration, no webpack issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_192533) do
+ActiveRecord::Schema.define(version: 2021_01_29_211351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_192533) do
     t.string "display_phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
   create_table "crawl_bars", force: :cascade do |t|
